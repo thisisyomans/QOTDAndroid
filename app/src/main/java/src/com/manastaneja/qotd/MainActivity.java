ea65c69526116ac7aca42f3436cb4def7c0f14c2
thisisyomans <tanejamm@gmail.com>
2018-12-07 10:11:29 -0800

    more quotes

--- app/src/main/java/src/com/manastaneja/qotd/MainActivity.java
+++ app/src/main/java/src/com/manastaneja/qotd/MainActivity.java
@@ -111,7 +111,7 @@ public class MainActivity extends AppCompatActivity {
             infoText.append(lines2.get(currentCSVLine));
         }
 
-        if (currentCSVLine == 172){
+        if (currentCSVLine == 199){
             currentCSVLine = 0;
         } else {
             currentCSVLine++;
